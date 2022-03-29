cb120afdd6bcdc03d7ec80a144805c779fe4fffc
Daniel Ribuffo <dribuffo@gmail.com>
2022-03-29 16:39:03 -0700

    added psuedocode for Routing and Link

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+//dependencies
+import React from 'react';
+import { Route, Routes, Link, Navigate } from "react-router-dom";
 import './App.css';
+
+//components import
 
 function App() {
   return (
     <div className="App">
-      
+      <nav>
+        {/* links to the different pages go here */}
+      </nav>
+      <main>
+        {/* Routes go here. */}
+      </main>
     </div>
   );
 }
