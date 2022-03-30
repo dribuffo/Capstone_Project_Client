570c69c0e8e99372813d3c06360433aa55cea0fc
Daniel Ribuffo <dribuffo@gmail.com>
2022-03-30 13:34:54 -0700

    added blue mage page

---
+++ src/components/BLU/BLU.js
@@ -0,0 +1,13 @@
+//dependencies
+import React from "react";
+import "./BLU.css";
+
+function BLU() {
+    return(
+        <div>
+            <h1>BLU Test</h1>
+        </div>
+    )
+}
+
+export default BLU;
