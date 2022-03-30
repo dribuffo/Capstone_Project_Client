0c2f9decc562b94d1f86101a3b5133264a528287
Daniel Ribuffo <dribuffo@gmail.com>
2022-03-30 13:36:14 -0700

    added Mounts page

---
+++ src/components/Mounts/Mounts.js
@@ -0,0 +1,13 @@
+//dependencies
+import React from "react";
+import "./Mounts.css";
+
+function Mounts() {
+    return(
+        <div>
+            <h1>Mounts Test</h1>
+        </div>
+    )
+}
+
+export default Mounts;
