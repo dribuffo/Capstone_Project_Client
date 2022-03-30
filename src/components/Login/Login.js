c05d02d3d9775a45d13baf51b3b1b437fed848f9
Daniel Ribuffo <dribuffo@gmail.com>
2022-03-30 16:57:39 -0700

    Added signup and login in front ends and applied light styling

---
+++ src/components/Login/Login.js
@@ -0,0 +1,78 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import "./Login.css";
+
+const Signup = () => {
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+  });
+  const [error, setError] = useState("");
+
+  const handleChange = ({ currentTarget: input }) => {
+    setData({ ...data, [input.email]: input.value });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const url = "http://localhost:4000/api/auth";
+      const { data: res } = await axios.post(url, data);
+      localStorage.setItem("token", res.data);
+      window.location = "/";
+    } catch (error) {
+      if (
+        error.response &&
+        error.response.status >= 400 &&
+        error.response.status <= 400
+      ) {
+        setError(error.response.data.message);
+      }
+    }
+  };
+
+  return (
+    <div className="login">
+      <div className="login_form">
+        <div className="left">
+          <form className="form" onSubmit={handleSubmit}>
+            <h1>Login to Database</h1>
+            <input
+              type="text"
+              placeholder="email"
+              name="email"
+              onChange={handleChange}
+              value={data.email}
+              required
+              className="input"
+            />
+            <input
+              type="password"
+              placeholder="password"
+              name="password"
+              onChange={handleChange}
+              value={data.password}
+              required
+              className="input"
+            />
+            {error && <div className="error_msg">{error}</div>}
+            <button type="submit" className="white_btn">
+              Sign In
+            </button>
+          </form>
+        </div>
+        <div className="right">
+          <h1>New Here?</h1>
+          <Link to="/login">
+            <button type="button" className="green_btn">
+              Sign Up
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
---
+++ src/components/Signup/Singup.js
@@ -0,0 +1,79 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import "./Signup.css";
+
+const Signup = () => {
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+  });
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const handleChange = ({ currentTarget: input }) => {
+    setData({ ...data, [input.email]: input.value });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const url = "http://localhost:4000/api/users";
+      const { data: res } = await axios.post(url, data);
+      navigate("/login");
+      console.log(res.message);
+    } catch (error) {
+      if (
+        error.response &&
+        error.response.status >= 400 &&
+        error.response.status <= 400
+      ) {
+        setError(error.response.data.message);
+      }
+    }
+  };
+
+  return (
+    <div className="signup">
+      <div className="signup_form">
+        <div className="left">
+          <h1>Welcome Back</h1>
+          <Link to="/login">
+            <button type="button" className="white_btn">
+              Sign In
+            </button>
+          </Link>
+        </div>
+        <div className="right">
+          <form className="form" onSubmit={handleSubmit}>
+            <h1>Create Account</h1>
+            <input
+              type="text"
+              placeholder="email"
+              name="email"
+              onChange={handleChange}
+              value={data.email}
+              required
+              className="input"
+            />
+            <input
+              type="password"
+              placeholder="password"
+              name="password"
+              onChange={handleChange}
+              value={data.password}
+              required
+              className="input"
+            />
+            {error && <div className="error_msg">{error}</div>}
+            <button type="submit" className="green_btn">
+              Sign Up
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
