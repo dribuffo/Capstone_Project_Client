130344fbc83345c6c5389c38af55ae7e83db0e14
Daniel Ribuffo <dribuffo@gmail.com>
2022-03-30 09:37:39 -0700

    deleted header in return for simple nav bar, updated App.js with navbar and home page route

--- src/App.js
+++ src/App.js
@@ -3,16 +3,40 @@ import React from 'react';
 import { Route, Routes, Link, Navigate } from "react-router-dom";
 import './App.css';
 
+//bootstrap imports
+import { Navbar, NavDropdown, Nav, Container } from 'react-bootstrap';
+
 //components import
+import Home from "./components/Home/Home";
 
 function App() {
   return (
     <div className="App">
-      <nav>
-        {/* links to the different pages go here */}
-      </nav>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">Lex Talionis Guild Database</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">View Mounts</Nav.Link>
+              <Nav.Link href="#link">View BLUs & Spells</Nav.Link>
+              <NavDropdown title="Admin" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Update Character</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">Log In</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
       <main>
-        {/* Routes go here. */}
+      <Routes>
+          <Route path="/" element={<Home />} />,
+          {/* <Route path="/CharacterDisplay" element={<CharacterDisplay />} />,
+          <Route path="/CharacterUpdate" element={<CharacterUpdate />} />,
+          <Route path="/AboutUs" element={<AboutUs />} />
+          <Route path="/Spells" element={<Spells />} /> */}
+      </Routes>
       </main>
     </div>
   );
