7e9f1449d56e7ef223cfab40a70c143f5560dcf7
Daniel Ribuffo <dribuffo@gmail.com>
2022-03-30 09:36:36 -0700

    Added Home Page Component

---
+++ src/components/Home/Home.js
@@ -0,0 +1,13 @@
+//dependencies
+import React from "react";
+import "./Home.css";
+
+function Home() {
+    return(
+        <div>
+            <h1>Home Page Test</h1>
+        </div>
+    )
+}
+
+export default Home;
