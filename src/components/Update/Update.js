8d36a9763e38745a90edfd1298f49079d4fab948
Daniel Ribuffo <dribuffo@gmail.com>
2022-03-31 15:39:40 -0700

    added Update component

---
+++ src/components/Update/Update.js
@@ -0,0 +1,13 @@
+//dependencies
+import React from "react";
+import "./Update.css";
+
+function Update() {
+    return(
+        <div>
+            <h1>Update Test</h1>
+        </div>
+    )
+}
+
+export default Update;
