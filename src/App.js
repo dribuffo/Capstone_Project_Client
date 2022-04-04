eb221e94b0f34f2cc27d0cd6113cc74c5813342d
Daniel Ribuffo <dribuffo@gmail.com>
2022-04-03 19:38:17 -0700

    deleted footer component, added footer information to footer tag using REACT bootstrap nav component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 //dependencies
-import React from 'react';
+import React, { useState } from 'react';
 import { Route, Routes, Link } from "react-router-dom";
 import './App.css';
 
@@ -8,57 +8,124 @@ import { Navbar, NavDropdown, Nav, Container } from 'react-bootstrap';
 
 //components import
 import Home from "./components/Home/Home";
-import Footer from "./components/Footer/Footer";
 import BLU from "./components/BLU/BLU";
 import Mounts from "./components/Mounts/Mounts";
 import Login from "./components/Login/Login";
 import Signup from "./components/Signup/Singup";
 import Update from './components/Update/Update';
 
+//footer links and imports
+import guildCrest from "./components/images/crest.png"
+import github from "./components/images/github.png"
+import linkedin from "./components/images/linkedin.png"
+import characterImage from "./components/images/DisplayNameSettings.png"
+let guildSite = 'https://na.finalfantasyxiv.com/lodestone/freecompany/9236179148295113228/';
+let githubLink = 'https://github.com/dribuffo'
+let linkedinLink = 'https://www.linkedin.com/in/danielribuffo/'
+let myCharacter = 'https://na.finalfantasyxiv.com/lodestone/character/210852/'
+
+
 
 function App() {
+  // **USE STATES**
+
+  // **HANDLE FUNCTIONS**
   const handleLogout = () => {
     localStorage.removeItem("token");
     window.location.reload();
-  }
+  };
 
-  const user = localStorage.getItem("token")
-  // Put logout button somewhere...
-  // <button className="white_btn" onClick={handleLogout}>Logout</button>
+  // **TOKEN**
+  const user = localStorage.getItem("token");
 
   return (
     <div className="App">
+      {/* NAVBAR */}
       <Navbar bg="light" expand="lg">
         <Container>
           <Navbar.Brand href="/">Lex Talionis Guild Database</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link as={Link} to={"/mounts"}>View Mounts</Nav.Link>
-              <Nav.Link as={Link} to={"/BLU"}>View BLUs and Spells</Nav.Link>
+              <Nav.Link as={Link} to={"/mounts"}>
+                View Mounts
+              </Nav.Link>
+              <Nav.Link as={Link} to={"/BLU"}>
+                View BLUs and Spells
+              </Nav.Link>
               <NavDropdown title="Admin" id="basic-nav-dropdown">
-                <NavDropdown.Item as={Link} to={"/update"}>Update Character</NavDropdown.Item>
+                <NavDropdown.Item as={Link} to={"/update"}>
+                  Update Character
+                </NavDropdown.Item>
                 <NavDropdown.Divider />
-                <NavDropdown.Item as={Link} to={"/Signup"}>Sign Up</NavDropdown.Item>
-                <NavDropdown.Item as={Link} to={"/Login"}>Log In</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <button className="white_btn" onClick={handleLogout}>Logout</button>
+                <NavDropdown.Item as={Link} to={"/Signup"}>
+                  Sign Up
+                </NavDropdown.Item>
+                <NavDropdown.Item as={Link} to={"/Login"}>
+                  Log In
+                </NavDropdown.Item>
+                {user ? (
+                  <button className="white_btn" onClick={handleLogout}>
+                    Logout
+                  </button>
+                ) : null}
               </NavDropdown>
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
+      {/* MAIN DIV USED FOR ROUTES */}
       <main>
-      <Routes>
+        <Routes>
           <Route path="/" element={<Home />} />,
           <Route path="/mounts" element={<Mounts />} />,
           <Route path="/BLU" element={<BLU />} />,
-          {user && <Route path="/update" element={<Update />} />}
+          {user && <Route path="/update" element={<Update />} />},
           <Route path="/Login" element={<Login />} />
           <Route path="/Signup" element={<Signup />} />
-      </Routes>
+        </Routes>
+        {/* <div className = "push"></div> */}
       </main>
-      <Footer />
+      {/* FOOTER */}
+      <footer>
+        <Nav className="justify-content-center" activeKey="/home">
+          <Nav.Item>
+            <Nav.Link href={guildSite}>
+              {" "}
+              <img className="iFoot" src={guildCrest} alt="guild crest" />
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey={myCharacter}>
+              <img
+                className="iFoot"
+                src={characterImage}
+                alt="Zebix Rune of Balmung"
+              />
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            {/* Github icons created by Pixel perfect - Flaticon */}
+            <Nav.Link a href={githubLink}>
+              <img
+                className="iFoot"
+                src={github}
+                alt="Github icons created by Pixel perfect - Flaticon"
+              />
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            {/* Linkedin icons created by riajulislam - Flaticon */}
+            <Nav.Link href={linkedinLink}>
+              <img
+                className="iFoot"
+                src={linkedin}
+                alt="Linkedin icons created by riajulislam - Flaticon"
+              />
+            </Nav.Link>
+          </Nav.Item>
+        </Nav>
+      </footer>
     </div>
   );
 }
