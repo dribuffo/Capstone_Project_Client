a25f73b0dc8031b14f7a1348721f71c288b7e7d2
Daniel Ribuffo <dribuffo@gmail.com>
2022-04-04 17:49:54 -0700

    added Add component for adding a new player

---
+++ src/components/Add/Add.js
@@ -0,0 +1,20 @@
+//dependencies
+import React from "react";
+import "./Add.css";
+
+//imports
+
+function Add() {
+
+    return(
+        <div>
+            <h1>Add Player Page</h1>
+            <h2>Step 1: Add Player by typing name in the empty field and pressing add.</h2>
+            placeholder stuff 
+            <h2>Step 2: Press button below to move to update page to update their information.</h2>
+            placeholder for button
+        </div>
+    )
+}
+
+export default Add;
